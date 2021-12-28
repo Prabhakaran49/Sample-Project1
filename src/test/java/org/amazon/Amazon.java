d98955c3835636fb303e445659ecf6f9f3bc5e8c
Prabhakaran49 <prabhakaranmb95@gmail.com>
2021-12-28 06:35:00 +0530

    Amazon-1 Code

--- src/test/java/org/amazon/Amazon.java
+++ src/test/java/org/amazon/Amazon.java
@@ -2,61 +2,82 @@ package org.amazon;
 
 import java.util.List;
 
+import org.apache.poi.ss.formula.functions.Replace;
 import org.base.ne.BaseClass;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.pom.Home_Page;
 
 public class Amazon extends BaseClass {
-public static void main(String[] args) throws InterruptedException {
-	chromeLaunch();
-	urlLaunch("https://www.amazon.in/ref=as_li_ss_tl?ie=UTF8&linkCode=ll2&tag=enin-edge-topsites-curate-ana-21&linkId=fbedcb44d04a4bae8eae32722a2f41c2&language=en_IN");
-	impWait(10);
-	
-	Home_Page hp = new Home_Page();
-	sendKeys(hp.getSearchtext(), "iphone 8");
-	click(hp.getSearchbutton());
-	
-	List<WebElement> allproduts = driver.findElements(By.xpath("//span[text()='New Apple iPhone 12 (128GB) - Green']"));
+	public static void main(String[] args) throws InterruptedException {
+		chromeLaunch();
+		urlLaunch("https://www.amazon.in/ref=as_li_ss_tl?ie=UTF8&linkCode=ll2&tag=enin-edge-topsites-curate-ana-21&linkId=fbedcb44d04a4bae8eae32722a2f41c2&language=en_IN");
+		impWait(10);
 
-	for (int i = 0; i < args.length; i++) {
+		Home_Page hp = new Home_Page();
+		sendKeys(hp.getSearchtext(), "iphone 8");
+		click(hp.getSearchbutton());
+
+				List<WebElement> productlist = driver.findElements(By.xpath("//span[@class='a-size-medium a-color-base a-text-normal']"));
 		
-	
+				for (WebElement e : productlist) {
+					System.out.println(e.getAttribute("innerText"));
+					System.out.println("------------------------------------");
+
+		}
+		List<WebElement> pricelist = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
+
 		
-	
-	WebElement p1 = allproduts.get(i);
-	String text = p1.getText();
-	System.out.println(text);
-	
-	
+			
+		
+		WebElement oneprice = pricelist.get(8);
+		String attribute = oneprice.getAttribute("innerText");
+		System.out.println(attribute);
+
+		if( attribute.contains(",")) {
+
+			String replace = attribute.replace(",", "");
+			
+			System.out.println(replace);
+
+
+		} 
+
+		
 	}
-	
-	
-	System.out.println("DONE");
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+
+
+
+
+
+
+
+
+	//		for (WebElement e : pricelist) {
+	//			System.out.println(e.getText());
+	//
+	//		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}	
+
